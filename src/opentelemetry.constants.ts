d37fc28c7bc3944399cda157a6ee87a5b83919e3
Ivan Santos <pragmaticivan@gmail.com>
2021-07-14 07:53:40 -0500

    feat: remove unused constant and reorganize pkgs

--- src/opentelemetry.constants.ts
+++ src/opentelemetry.constants.ts
@@ -1,7 +1,3 @@
-export enum Constants {
-  METRIC_EXPORTER = 'OPEN_TELEMETRY_SDK_METRIC_EXPORTER',
-}
-
 export const OPENTELEMETRY_SDK = 'OPEN_TELEMETRY_SDK';
 export const OPENTELEMETRY_MODULE_OPTIONS = 'OpenTelemetryModuleOptions';
 export const OPENTELEMETRY_MODULE_ID = 'OpenTelemetryModuleId';
