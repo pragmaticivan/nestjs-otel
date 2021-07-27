66bf956806a3bf07d6c1027ecb2f706376cc5fce
argens <argens@spaceshipapp.com>
2021-07-27 18:17:07 +0800

    fix(decorator): should set spawned span as active

--- src/tracing/decorators/span.ts
+++ src/tracing/decorators/span.ts
@@ -9,13 +9,12 @@ export function Span(name?: string) {
     const method = propertyDescriptor.value;
     // eslint-disable-next-line no-param-reassign
     propertyDescriptor.value = function PropertyDescriptor(...args: any[]) {
-      const currentSpan = trace.getSpan(context.active());
       const tracer = trace.getTracer('default');
+      const span = tracer.startSpan(
+        name || `${target.constructor.name}.${propertyKey}`,
+      );
 
-      return context.with(trace.setSpan(context.active(), currentSpan), () => {
-        const span = tracer.startSpan(
-          name || `${target.constructor.name}.${propertyKey}`,
-        );
+      return context.with(trace.setSpan(context.active(), span), () => {
         if (method.constructor.name === 'AsyncFunction') {
           return method.apply(this, args).finally(() => {
             span.end();
