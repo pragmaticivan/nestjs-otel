6390e76e8f7acda1a7ae5afd3780c46057766576
Ivan Santos <pragmaticivan@gmail.com>
2021-07-27 19:32:48 -0500

    chore: update examples for v2

--- examples/nestjs-prom-grafana-tempo/src/app.module.ts
+++ examples/nestjs-prom-grafana-tempo/src/app.module.ts
@@ -2,18 +2,6 @@ import { Module } from '@nestjs/common';
 import { OpenTelemetryModule } from 'nestjs-otel';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AsyncLocalStorageContextManager } from '@opentelemetry/context-async-hooks';
-import {
-  CompositePropagator,
-  HttpTraceContextPropagator,
-  HttpBaggagePropagator,
-} from '@opentelemetry/core';
-import { BatchSpanProcessor } from '@opentelemetry/tracing';
-import { JaegerExporter } from '@opentelemetry/exporter-jaeger';
-import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
-import { JaegerPropagator } from '@opentelemetry/propagator-jaeger';
-import { B3InjectEncoding, B3Propagator } from '@opentelemetry/propagator-b3';
-import { PrometheusExporter } from '@opentelemetry/exporter-prometheus';
 import { LoggerModule } from './logger/logger.module';
 
 const OpenTelemetryModuleConfig = OpenTelemetryModule.forRoot({
@@ -22,27 +10,7 @@ const OpenTelemetryModuleConfig = OpenTelemetryModule.forRoot({
     defaultMetrics: true,
     apiMetrics: {
       enable: true,
-      timeBuckets: [],
     },
-  },
-  nodeSDKConfiguration: {
-    metricExporter: new PrometheusExporter({
-      port: 8081,
-    }),
-    spanProcessor: new BatchSpanProcessor(new JaegerExporter()),
-    contextManager: new AsyncLocalStorageContextManager(),
-    textMapPropagator: new CompositePropagator({
-      propagators: [
-        new JaegerPropagator(),
-        new HttpTraceContextPropagator(),
-        new HttpBaggagePropagator(),
-        new B3Propagator(),
-        new B3Propagator({
-          injectEncoding: B3InjectEncoding.MULTI_HEADER,
-        }),
-      ],
-    }),
-    instrumentations: [getNodeAutoInstrumentations()],
   },
 });
 
