18d2964f92201cc14dd5ea43e90b1bf63f019bc6
Tine Jozelj <tine@tjo.space>
2022-03-29 17:58:46 +0200

    chore: add helper util for metadata copying

---
+++ src/opentelemetry.utils.ts
@@ -0,0 +1,15 @@
+export const copyMetadataFromFunctionToFunction = (
+  originalFunction: Function,
+  newFunction: Function,
+): void => {
+  // Get the current metadata and set onto the wrapper
+  // to ensure other decorators ( ie: NestJS EventPattern / RolesGuard )
+  // won't be affected by the use of this instrumentation
+  Reflect.getMetadataKeys(originalFunction).forEach(metadataKey => {
+    Reflect.defineMetadata(
+      metadataKey,
+      Reflect.getMetadata(metadataKey, originalFunction),
+      newFunction,
+    );
+  });
+};
