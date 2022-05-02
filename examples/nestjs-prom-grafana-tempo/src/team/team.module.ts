9d8a4fec80d3a05a037612d63ee8d7eba63b0d68
Ivan Santos <301291+pragmaticivan@users.noreply.github.com>
2022-05-01 22:04:34 -0500

    chore: move nestjs-prom-grafana-tempo example

--- examples/nestjs-prom-grafana-tempo/src/team/team.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TeamService } from './team.service';
-import { TeamController } from './team.controller';
-
-@Module({
-  providers: [TeamService],
-  controllers: [TeamController],
-})
-export class TeamModule {}
