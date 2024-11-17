916c8ec29fe4529868f30cc7f19ffd78f961ff90
Ivan Santos <301291+pragmaticivan@users.noreply.github.com>
2024-11-17 08:35:39 -0600

    chore: simplify commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,3 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
-  rules: {
-    'scope-case': [2, 'always', ['sentence-case', 'start-case', 'pascal-case', 'upper-case']],
-    'subject-case': [2, 'never', ['start-case', 'pascal-case']],
-  },
 };
