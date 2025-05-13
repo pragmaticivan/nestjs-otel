7cb53d774de31cd01868d26482c606ef37c947dc
Ondřej Švanda <ondrej.svanda@iot-water.cz>
2025-05-13 17:47:12 +0200

    refactor: add explicit types to Span decorator

--- src/tracing/decorators/span.ts
+++ src/tracing/decorators/span.ts
@@ -6,33 +6,39 @@ const recordException = (span: ApiSpan, error: any) => {
   span.setStatus({ code: SpanStatusCode.ERROR, message: error.message });
 };
 
-export function Span(name?: string, options: SpanOptions = {}) {
-  return (target: any, propertyKey: PropertyKey, propertyDescriptor: PropertyDescriptor) => {
+export function Span<T extends any[]>(name?: string, options: SpanOptions = {}) {
+  return (
+    target: any,
+    propertyKey: PropertyKey,
+    propertyDescriptor: TypedPropertyDescriptor<(...args: T) => any>
+  ) => {
     const originalFunction = propertyDescriptor.value;
-    const wrappedFunction = function PropertyDescriptor(...args: any[]) {
+
+    if (typeof originalFunction !== 'function') {
+      throw new Error(
+        `The @Span decorator can be only used on functions, but ${propertyKey.toString()} is not a function.`
+      );
+    }
+
+    const wrappedFunction = function PropertyDescriptor(this: any, ...args: T) {
       const tracer = trace.getTracer('default');
       const spanName = name || `${target.constructor.name}.${String(propertyKey)}`;
 
       return tracer.startActiveSpan(spanName, options, span => {
         if (originalFunction.constructor.name === 'AsyncFunction') {
-          return (
-            originalFunction
-              // @ts-ignore
-              .apply(this, args)
-              // @ts-ignore
-              .catch(error => {
-                recordException(span, error);
-                // Throw error to propagate it further
-                throw error;
-              })
-              .finally(() => {
-                span.end();
-              })
-          );
+          return originalFunction
+            .apply(this, args)
+            .catch((error: any) => {
+              recordException(span, error);
+              // Throw error to propagate it further
+              throw error;
+            })
+            .finally(() => {
+              span.end();
+            });
         }
 
         try {
-          // @ts-ignore
           return originalFunction.apply(this, args);
         } catch (error) {
           recordException(span, error);
